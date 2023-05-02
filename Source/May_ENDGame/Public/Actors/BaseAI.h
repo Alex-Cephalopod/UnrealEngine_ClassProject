7b3c7c1cf93623b37bf783d1fd27a46183660ed8
Alex-Cephalopod <76233802+Alex-Cephalopod@users.noreply.github.com>
2023-05-02 19:57:31 -0400

    Almost finishing Health Component

---
+++ Source/May_ENDGame/Public/Actors/BaseAI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/BaseCharacter.h"
+#include "BaseAI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAY_ENDGAME_API ABaseAI : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
